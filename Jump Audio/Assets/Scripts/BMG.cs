6ca57f3b8f6c71d7a9f18f376b910bd1d7a2a81a
Itzuzume <ellson637@gmail.com>
2021-01-13 16:25:40 +0800

    23_Ellson_Lab15

---
+++ Jump Audio/Assets/Scripts/BMG.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BMG : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    public AudioClip[] AudioClipArr;
+
+
+    float Increasevolume = 0.6f;
+    float Decreasevolume = 0.6f;
+    float VolumeRate = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+
+            int rand = Random.Range(0, AudioClipArr.Length);
+            audioSource.PlayOneShot(AudioClipArr[rand]);
+        }
+
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Increasevolume = Increasevolume + (VolumeRate * Time.deltaTime);
+            GetComponent<AudioSource>().volume = Increasevolume;
+        }
+
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Decreasevolume = Decreasevolume - (VolumeRate * Time.deltaTime);
+            GetComponent<AudioSource>().volume = Decreasevolume;
+            
+        }
+    }
+}
---
+++ Jump Audio/Assets/Scripts/Playercontroller.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Playercontroller : MonoBehaviour
+{
+
+    public GameObject JumpText;
+
+    private int jumpCount;
+    private Rigidbody rb;
+
+    public bool Isground;
+
+    public Vector3 jump;
+    public float Jumpforce = 2.0f;
+
+    private AudioSource audioSource;
+
+    public AudioClip[] AudioClipArr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
+    void OnCollisionStay()
+    {
+        Isground = true;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && Isground)
+        {
+            jumpCount++;
+
+            JumpText.GetComponent<Text>().text = "Jump: " + jumpCount;
+
+            rb.AddForce(jump * Jumpforce, ForceMode.Impulse);
+            Isground = false;
+
+            int rand = Random.Range(0, AudioClipArr.Length);
+            audioSource.PlayOneShot(AudioClipArr[rand]);
+
+            //audioSource.Play();
+
+
+        }
+    }
+}
